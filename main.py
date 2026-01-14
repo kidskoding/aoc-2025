1961a04eb620618d14638ccd7f6654964204b205
Anirudh Konidala <kidskodingclub@gmail.com>
2026-01-13 18:51:46 -0600

    completed day 5 part 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.prob00 import prob00
 from src.prob01 import prob1_1, prob1_2
 from src.prob02 import prob2_1, prob2_2
 from src.prob03 import prob3_1, prob3_2
-from src.prob05 import prob5_1
+from src.prob05 import prob5_1, prob5_2
 
 # print(f'Problem 0: {prob00()}\n')
 
@@ -12,7 +12,8 @@ from src.prob05 import prob5_1
 # print(f'Problem 2 Part 1: {prob2_1()}')
 # print(f'Problem 2 Part 2: {prob2_2()}')
 
-print(f'Problem 3 Part 1: {prob3_1()}')
-print(f'Problem 3 Part 2: {prob3_2()}')
+# print(f'Problem 3 Part 1: {prob3_1()}')
+# print(f'Problem 3 Part 2: {prob3_2()}')
 
 print(f'Problem 5 Part 1: {prob5_1()}')
+print(f'Problem 5 Part 2: {prob5_2()}')
--- src/prob05.py
+++ src/prob05.py
@@ -15,3 +15,24 @@ def prob5_1():
                     break
             
     return fresh_ingredients
+
+def prob5_2():
+    ranges = []
+    with open('./input/prob05.txt') as f:
+        line = f.readline().strip()
+        while line != '' and '-' in line:
+            start = int(line[0:line.index('-')])
+            end = int(line[line.index('-') + 1:])
+            
+            ranges.append((start, end))
+            line = f.readline().strip()
+    
+    ranges.sort()
+    merged = []
+    for start, end in ranges:
+        if merged and start <= merged[-1][1] + 1:
+            merged[-1] = (merged[-1][0], max(merged[-1][1], end))
+        else:
+            merged.append((start, end))
+    
+    return sum(end - start + 1 for start, end in merged)
