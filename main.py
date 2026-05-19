d878232081b50d0b3bee5c71423b7c29c6e47b88
Anirudh Konidala <kidskodingclub@gmail.com>
2026-05-19 10:23:39 -0500

    completed problem 4 pt 1 and 2

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ from src.prob04 import prob4_1, prob4_2
 from src.prob05 import prob5_1, prob5_2
 from src.prob06 import prob6_1, prob6_2
 
-print(f'Problem 0: {prob00()}\n')
+# print(f'Problem 0: {prob00()}\n')
 
-print(f'Problem 1 Part 1: {prob1_1()}')
-print(f'Problem 1 Part 2: {prob1_2()}\n')
+# print(f'Problem 1 Part 1: {prob1_1()}')
+# print(f'Problem 1 Part 2: {prob1_2()}\n')
 
-print(f'Problem 2 Part 1: {prob2_1()}')
-print(f'Problem 2 Part 2: {prob2_2()}\n')
+# print(f'Problem 2 Part 1: {prob2_1()}')
+# print(f'Problem 2 Part 2: {prob2_2()}\n')
 
-print(f'Problem 3 Part 1: {prob3_1()}')
-print(f'Problem 3 Part 2: {prob3_2()}\n')
+# print(f'Problem 3 Part 1: {prob3_1()}')
+# print(f'Problem 3 Part 2: {prob3_2()}\n')
 
-print(f'Problem 4 Part 1: {prob4_1()}')
+# print(f'Problem 4 Part 1: {prob4_1()}')
 print(f'Problem 4 Part 2: {prob4_2()}\n')
 
-print(f'Problem 5 Part 1: {prob5_1()}')
-print(f'Problem 5 Part 2: {prob5_2()}\n')
+# print(f'Problem 5 Part 1: {prob5_1()}')
+# print(f'Problem 5 Part 2: {prob5_2()}\n')
 
-print(f'Problem 6 Part 1: {prob6_1()}')
+# print(f'Problem 6 Part 1: {prob6_1()}')
 # print(f'Problem 6 Part 2: {prob6_2()}\n')
--- src/prob04.py
+++ src/prob04.py
@@ -33,4 +33,45 @@ def prob4_1() -> int:
     return accessible_rolls
 
 def prob4_2() -> int:
-    pass
+    grid = []
+    
+    with open('./input/prob04.txt') as f:
+        for line in f:
+            grid.append(list(line.strip()))
+    
+    directions = [
+        (-1, 1), (-1, 0), (-1, -1),
+        (0, -1),          (0, 1),
+        (1, -1),  (1, 0), (1, 1)
+    ]
+    rows = len(grid)
+    cols = len(grid[0])
+    
+    accessible_rolls = 0
+    while True:
+        to_remove = []
+        
+        for r in range(rows):
+            for c in range(cols):
+                neighbor_count = 0
+                
+                if grid[r][c] == '@':
+                    for dr, dc in directions:
+                        nr = r + dr
+                        nc = c + dc
+                        
+                        if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == '@':
+                            neighbor_count += 1
+                            
+                    if neighbor_count < 4:
+                        to_remove.append((r, c))
+                        
+        if not to_remove:
+            break
+        
+        accessible_rolls += len(to_remove)
+        
+        for (r, c) in to_remove:
+            grid[r][c] = '.'
+            
+    return accessible_rolls
