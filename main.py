34fd1bd6d3aa4959ea033b4cf9d56f1ae3c4b110
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-08-12 00:59:54 -0500

    added prob 08 part 2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.prob03 import prob03_1, prob03_2
 from src.prob04 import prob04_1, prob04_2
 from src.prob05 import prob05_1, prob05_2
 from src.prob06 import prob06_1, prob06_2
-from src.prob08 import prob08_1
+from src.prob08 import prob08_1, prob08_2
 from src.prob09 import prob09_1, prob09_2
 from src.prob11 import prob11_1, prob11_2
 
@@ -30,6 +30,7 @@ from src.prob11 import prob11_1, prob11_2
 # print(f'Problem 6 Part 2: {prob06_2()}\n')
 
 print(f'Problem 8 Part 1: {prob08_1()}')
+print(f'Problem 8 Part 2: {prob08_2()}')
 
 # print(f'Problem 9 Part 1: {prob09_1()}')
 # print(f'Problem 9 Part 2: {prob09_2()}\n')
--- src/prob08.py
+++ src/prob08.py
@@ -2,6 +2,22 @@ import heapq
 import itertools
 from collections import Counter
 
+class DisjointSet:
+    def find(self, x: int) -> int:
+        while parent[x] != x:
+            parent[x] = parent[parent[x]]
+            x = parent[x]
+
+        return x
+
+    def union(self, u: int, v: int) -> bool:
+        ru, rv = self.find(u), self.find(v)
+        if ru == rv:
+            return False
+
+        parent[ru] = rv
+        return True
+
 def prob08_1() -> int:
     points = []
     with open('./input/prob08.txt') as f:
@@ -14,29 +30,43 @@ def prob08_1() -> int:
     n = len(points)
     global parent
     parent = list(range(n))
-    
-    def find(x: int) -> int:
-        while parent[x] != x:
-            parent[x] = parent[parent[x]]
-            x = parent[x]
-
-        return x
-
-    def union(u: int, v: int):
-        ru, rv = find(u), find(v)
-        if ru == rv:
-            return
-        else:
-            parent[ru] = parent[rv]
 
     edges = [(sum((a - b) ** 2 for a, b in zip(points[i], points[j])), i, j) for i, j in itertools.combinations(range(n), 2)]
     edges.sort()
+    dset = DisjointSet()
     for d2, i, j in edges[:1000]:
-        union(i, j)
+        dset.union(i, j)
 
-    sizes = Counter(find(i) for i in range(n))
+    sizes = Counter(dset.find(i) for i in range(n))
     total = 1
     for root, count in sizes.most_common(3):
         total *= count
 
     return total
+
+def prob08_2() -> int:
+    points = []
+    with open('./input/prob08.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            coords = line.strip().split(',')
+            coords = tuple(int(x) for x in coords)
+            points.append(coords)
+
+    n = len(points)
+    global parent
+    parent = list(range(n))
+
+    # kruskal's:
+    edges = [(sum((a - b) ** 2 for a, b in zip(points[i], points[j])), i, j) for i, j in itertools.combinations(range(n), 2)]
+    edges.sort()
+    dset = DisjointSet()
+    components = n
+    for d2, i, j in edges:
+        if dset.union(i, j):
+            components -= 1
+
+        if components == 1:
+            return points[i][0] * points[j][0]
+
+    return -1
